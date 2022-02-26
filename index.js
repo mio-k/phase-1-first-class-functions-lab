7275ed10758804d296b29fee2c8b1014cf3fdad0
mio kataoka <mio.kanna@gmail.com>
2022-02-25 18:28:56 -0800

    updating the index.js with my struggle.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arr) {
+    return [arr[0], arr[1]];
+}
+const returnLastTwoDrivers = function(arr){
+    return [arr[arr.length-2], arr[arr.length-1]];
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+//function createFareMultiplier(num) {
+//    function anotherFunction {
+//        return num * num;
+//    }
+//} 
+
